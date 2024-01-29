1407cd06d3f1d2b12f23552cd29808faaffa2f12
Niklas Satzinger <niklas.satzinger@gmail.com>
2024-01-29 16:24:01 +0100

    deleted docs folder from main branch

--- docs/package-search-index.js
+++
@@ -1,5 +0,0 @@
-packageSearchIndex = [{
-	'l': 'All Packages',
-	'u': 'allpackages-index.html'
-}, {'l': 'at.htl.timetableGenerator'}, {'l': 'at.htl.timetableGenerator.constrains'}, {'l': 'at.htl.timetableGenerator.exceptions'}, {'l': 'at.htl.timetableGenerator.output'}];
-updateSearchResults();

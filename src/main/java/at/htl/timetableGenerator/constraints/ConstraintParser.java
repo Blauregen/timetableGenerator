1eb6a1261c9abd67107a4979661afe363757838f
TimMayr <tim.mayr@gmx.at>
2024-03-04 14:40:06 +0100

    Added Custom Constraints for defining max number of same subject in row
    Took 53 minutes
    
    (cherry picked from commit 57b7a36cc0c5a61a572c599d90785f4a66bd0ce8)
    
    Took 8 minutes

--- src/main/java/at/htl/timetableGenerator/constraints/ConstraintParser.java
+++ src/main/java/at/htl/timetableGenerator/constraints/ConstraintParser.java
@@ -1,9 +1,17 @@
 package at.htl.timetableGenerator.constraints;
 
-import at.htl.timetableGenerator.constraints.constraints.CustomConstraint;
+import at.htl.timetableGenerator.exceptions.ImportException;
 import at.htl.timetableGenerator.factory.CustomConstraintFactory;
+import at.htl.timetableGenerator.factory.MultipleConstraintFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class ConstraintParser {
 	private final String path;
@@ -12,7 +20,36 @@ public class ConstraintParser {
 		this.path = path;
 	}
 
-	public Set<CustomConstraint> parse() {
-		return CustomConstraintFactory.createFromFile(path);
+	public Set<Constraint> parse() {
+		try (Stream<String> lines = Files.lines(Paths.get(path))) {
+			HashSet<Constraint> constraints = new HashSet<>();
+
+			lines.forEach((line) -> {
+				for (TimeKeyword value : TimeKeyword.values()) {
+					Pattern pattern = Pattern.compile("\\b" + value + "\\b");
+					Matcher matcher = pattern.matcher(line.toUpperCase());
+
+					if (matcher.find()) {
+						constraints.add(CustomConstraintFactory.createFromString(line));
+						return;
+					}
+				}
+
+				for (DurationKeyword value : DurationKeyword.values()) {
+					Pattern pattern = Pattern.compile("\\b" + value + "\\b");
+					Matcher matcher = pattern.matcher(line.toUpperCase());
+
+					if (matcher.find()) {
+						constraints.add(MultipleConstraintFactory.createFromString(line));
+						return;
+					}
+				}
+			});
+
+
+			return constraints;
+		} catch (IOException e) {
+			throw new ImportException("Error reading File!", e);
+		}
 	}
 }

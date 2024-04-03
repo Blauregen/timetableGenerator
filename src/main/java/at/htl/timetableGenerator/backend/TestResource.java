e045f140942113cfdb19c55d9aec082af020e241
Tobias-Huch <tobias.huch@gmx.at>
2024-04-03 08:11:05 +0200

    Added API Paths for exportFormats and exportData

--- src/main/java/at/htl/timetableGenerator/backend/TestResource.java
+++ src/main/java/at/htl/timetableGenerator/backend/TestResource.java
@@ -1,24 +1,53 @@
 package at.htl.timetableGenerator.backend;
 
+import at.htl.timetableGenerator.output.ExportData;
+import at.htl.timetableGenerator.output.ExportFormat;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/test")
+import java.util.LinkedList;
+import java.util.List;
+
+@Path("/api")
 public class TestResource {
-	public class Tester {
-		public String blub;
-		public int meep;
+	public class multipleAnswersReturnObject {
+		public String[] answer;
 	}
 
 
 	@GET
+	@Path("/exportFormats")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Tester test(){
-		Tester tester = new Tester();
-		tester.blub = "Test";
-		tester.meep = 1;
-		return tester;
+	public multipleAnswersReturnObject exportFormats(){
+		multipleAnswersReturnObject result = new multipleAnswersReturnObject();
+
+		ExportFormat[] formats = ExportFormat.values();
+		List<String> exportsAsStrings = new LinkedList<>();
+
+		for (ExportFormat currentExportFormat : formats) {
+			exportsAsStrings.add(currentExportFormat.toString());
+		}
+
+		result.answer = exportsAsStrings.toArray(new String[0]);
+		return result;
+	}
+
+	@GET
+	@Path("/exportData")
+	@Produces(MediaType.APPLICATION_JSON)
+	public multipleAnswersReturnObject exportData(){
+		multipleAnswersReturnObject result = new multipleAnswersReturnObject();
+
+		ExportData[] exportData = ExportData.values();
+		List<String> exportsAsStrings = new LinkedList<>();
+
+		for (ExportData currentExportData : exportData) {
+			exportsAsStrings.add(currentExportData.toString());
+		}
+
+		result.answer = exportsAsStrings.toArray(new String[0]);
+		return result;
 	}
 }
